bf538ca43399fd3d4516610041f6a430049b3f1c
Mengrui Ni <mengrui_ni@brown.edu>
2015-04-12 22:17:26 -0400

    add documentation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PortfolioCalculator
+
+
+## Dependencies
+
+* NumPy 
+* Pandas
+* PySide
+
+
+## Documentation
--- forecastEngine.py
+++ forecastEngine.py
@@ -85,7 +85,8 @@ class ForecastEngine(QtGui.QWidget):
 
 	def browsePClicked(self):
 		f, _ = QtGui.QFileDialog.getOpenFileName(self)
-		if f != "":
+		# a little hack on the file name to avoid undesired imports
+		if f != "" and f.endswith("Portfolio"):
 			self.portEdit.setText(f)
 			self.portData = self.get_data(f)
 			self.portTable = self.create_table(self.portData)
@@ -96,7 +97,8 @@ class ForecastEngine(QtGui.QWidget):
 
 	def browseEClicked(self):
 		f, _ = QtGui.QFileDialog.getOpenFileName(self)
-		if f != "":
+		# a little hack on the file name to avoid undesired imports
+		if f != "" and f.endswith("MacroeconomicData"):
 			self.econEdit.setText(f)
 			self.econData = self.get_data(f)
 			self.econTable = self.create_table(self.econData)
@@ -117,14 +119,16 @@ class ForecastEngine(QtGui.QWidget):
 		self.balanceTable.resizeColumnsToContents()
 		self.stackLayout.addWidget(self.balanceTable)
 		self.stackLayout.setCurrentWidget(self.balanceTable)
+		self.runButton.setEnabled(0)
 
 	def balanceClicked(self, index):
 		self.balance = Balance(self.portData.values[index], self.econData)
-		#self.balance.setGeometry(200, 200, 900, 700)
 
 	# may need to write a special get_data function for portfolio data
 	# to avoid running out of memory
 	def get_data(self, filepath):
+		#iter_csv = pd.read_csv(filepath, iterator=True, chunksize=5)
+		#return pd.concat([chunk for chunk in iter_csv])
 		return pd.read_csv(filepath)
 
 	def create_table(self, dataFrame):
@@ -191,7 +195,6 @@ class Balance(QtGui.QWidget):
 				noise = np.random.normal(0, 1, 1)
 				rate[ii,tt] = rate[ii,tt-1]+a*(b-rate[ii,tt-1])+sigma*noise
 		return sum(rate)/1000.0
-
 
 	def createTable(self):
 		self.header = ["Date", "Normal Repayment", "P(Refinance)", "P(Default)", "Expected Balance"]
@@ -275,124 +278,107 @@ class Balance(QtGui.QWidget):
 
 
 
-class PortfolioTab(QtGui.QWidget):
-	def __init__(self, parent=None):
-		super(PortfolioTab, self).__init__(parent)
-		self.portfolio = self.get_data(filename="loanData")
-		self.initUI()
+# class PortfolioTab(QtGui.QWidget):
+# 	def __init__(self, parent=None):
+# 		super(PortfolioTab, self).__init__(parent)
+# 		self.portfolio = self.get_data(filename="loanData")
+# 		self.initUI()
 
-	def initUI(self):
-		header = self.portfolio.columns
-		data = self.portfolio.values
-		self.table = QtGui.QTableWidget(len(data), len(header))
+# 	def initUI(self):
+# 		header = self.portfolio.columns
+# 		data = self.portfolio.values
+# 		self.table = QtGui.QTableWidget(len(data), len(header))
 
-		for ii in range(len(header)):
-			item = QtGui.QTableWidgetItem(str(header[ii]))
-			self.table.setItem(0, ii, item)
-		for ii in range(len(data)):
-			for jj in range(len(header)):
-				item = QtGui.QTableWidgetItem(str(data[ii][jj]))
-				self.table.setItem(ii+1, jj, item) 
-		self.table.resizeColumnsToContents()
+# 		for ii in range(len(header)):
+# 			item = QtGui.QTableWidgetItem(str(header[ii]))
+# 			self.table.setItem(0, ii, item)
+# 		for ii in range(len(data)):
+# 			for jj in range(len(header)):
+# 				item = QtGui.QTableWidgetItem(str(data[ii][jj]))
+# 				self.table.setItem(ii+1, jj, item) 
+# 		self.table.resizeColumnsToContents()
 
-		mainLayout = QtGui.QHBoxLayout()
-		mainLayout.addWidget(self.table)
-		self.setLayout(mainLayout)
+# 		mainLayout = QtGui.QHBoxLayout()
+# 		mainLayout.addWidget(self.table)
+# 		self.setLayout(mainLayout)
 
-class MacroeconTab(QtGui.QWidget):
-	def __init__(self, parent=None):
-		super(MacroeconTab, self).__init__(parent)
-		self.econData = self.get_data(filename="econData")
-		self.initUI()
+# class MacroeconTab(QtGui.QWidget):
+# 	def __init__(self, parent=None):
+# 		super(MacroeconTab, self).__init__(parent)
+# 		self.econData = self.get_data(filename="econData")
+# 		self.initUI()
 
-	def initUI(self):
-		header = self.econData.columns
-		data = self.econData.values
-		self.table = QtGui.QTableWidget(len(data), len(header))
+# 	def initUI(self):
+# 		header = self.econData.columns
+# 		data = self.econData.values
+# 		self.table = QtGui.QTableWidget(len(data), len(header))
 
-		for ii in range(len(header)):
-			item = QtGui.QTableWidgetItem(str(header[ii]))
-			self.table.setItem(0, ii, item)
-		for ii in range(len(data)):
-			for jj in range(len(header)):
-				item = QtGui.QTableWidgetItem(str(data[ii][jj]))
-				self.table.setItem(ii+1, jj, item) 
-		#self.table.resizeColumnsToContents()
+# 		for ii in range(len(header)):
+# 			item = QtGui.QTableWidgetItem(str(header[ii]))
+# 			self.table.setItem(0, ii, item)
+# 		for ii in range(len(data)):
+# 			for jj in range(len(header)):
+# 				item = QtGui.QTableWidgetItem(str(data[ii][jj]))
+# 				self.table.setItem(ii+1, jj, item) 
+# 		#self.table.resizeColumnsToContents()
 
-		mainLayout = QtGui.QHBoxLayout()
-		mainLayout.addWidget(self.table)
-		self.setLayout(mainLayout)
+# 		mainLayout = QtGui.QHBoxLayout()
+# 		mainLayout.addWidget(self.table)
+# 		self.setLayout(mainLayout)
 
 
-	def get_data(self, filepath=".", filename="econData"):
-		fpath = os.path.join(filepath, filename)
-		return pd.read_csv(fpath)
+# 	def get_data(self, filepath=".", filename="econData"):
+# 		fpath = os.path.join(filepath, filename)
+# 		return pd.read_csv(fpath)
 
-class BalanceTab(QtGui.QWidget):
+# class BalanceTab(QtGui.QWidget):
 
-	def __init__(self, parent=None):
-		super(BalanceTab, self).__init__(parent)
-		self.econData = MacroeconTab().econData
-		self.portfolio = PortfolioTab().portfolio
-		self.tableLayout = QtGui.QStackedLayout()
-		self.initUI()
+# 	def __init__(self, parent=None):
+# 		super(BalanceTab, self).__init__(parent)
+# 		self.econData = MacroeconTab().econData
+# 		self.portfolio = PortfolioTab().portfolio
+# 		self.tableLayout = QtGui.QStackedLayout()
+# 		self.initUI()
 
-	def initUI(self):
-		header = self.portfolio.columns
-		data = self.portfolio.values
-		self.table = QtGui.QTableWidget(len(data), len(header)+1)
-		results = []
+# 	def initUI(self):
+# 		header = self.portfolio.columns
+# 		data = self.portfolio.values
+# 		self.table = QtGui.QTableWidget(len(data), len(header)+1)
+# 		results = []
 
-		for ii in range(len(header)):
-			item = QtGui.QTableWidgetItem(str(header[ii]))
-			self.table.setItem(0, ii, item)
-		for ii in range(len(data)):
-			for jj in range(len(header)):
-				item = QtGui.QTableWidgetItem(str(data[ii][jj]))
-				self.table.setItem(ii+1, jj, item) 
-			button = QtGui.QPushButton("View Balance")
-			results.append(button)
-			self.table.setCellWidget(ii+1, len(header), button)
-		self.table.resizeColumnsToContents()
+# 		for ii in range(len(header)):
+# 			item = QtGui.QTableWidgetItem(str(header[ii]))
+# 			self.table.setItem(0, ii, item)
+# 		for ii in range(len(data)):
+# 			for jj in range(len(header)):
+# 				item = QtGui.QTableWidgetItem(str(data[ii][jj]))
+# 				self.table.setItem(ii+1, jj, item) 
+# 			button = QtGui.QPushButton("View Balance")
+# 			results.append(button)
+# 			self.table.setCellWidget(ii+1, len(header), button)
+# 		self.table.resizeColumnsToContents()
 
-		self.label = QtGui.QLabel("")
+# 		self.label = QtGui.QLabel("")
 
 		
-		self.tableLayout.addWidget(self.label)
-		self.tableLayout.addWidget(self.table)
+# 		self.tableLayout.addWidget(self.label)
+# 		self.tableLayout.addWidget(self.table)
 
-		mainLayout = QtGui.QVBoxLayout(self)
-		mainLayout.addLayout(self.tableLayout)
+# 		mainLayout = QtGui.QVBoxLayout(self)
+# 		mainLayout.addLayout(self.tableLayout)
 
-		self.setLayout(mainLayout)
+# 		self.setLayout(mainLayout)
 
-	@staticmethod
-	def changeLayout(self):
-		self.tableLayout.setCurrentWidget(self.table)
+# 	@staticmethod
+# 	def changeLayout(self):
+# 		self.tableLayout.setCurrentWidget(self.table)
 
-	@classmethod
-	def hello(self):
-		print "hello"
+# 	@classmethod
+# 	def hello(self):
+# 		print "hello"
 
-
-# def get_data(filepath='.', filename='loanData', **kwargs):
-# 	fpath = os.path.join(filepath, filename)
-# 	return pd.read_csv(fpath)
-	#iter_csv = pd.read_csv(fpath, iterator=True, chunksize=5)
-	#return pd.concat([chunk[chunk['Loan_Channel'] == 'A'] for chunk in iter_csv])
-
-def calc_balance(portfolio):
-	balances = []
-	for loan in portfolio.values:
-		L = loan[3]
-		age = loan[9]
-		c = loan[7]/12.0
-		N = loan[6]
-	return balances
 
 def main():
-	#portfolio = get_data(filename='loanData')
-	#econData = get_data(filename='econData')
 	app = QtGui.QApplication(sys.argv)
 	calculator = ForecastEngine()
 	sys.exit(app.exec_())
@@ -400,3 +386,4 @@ def main():
 if __name__ == '__main__':
 	import os, sys
 	main()
+
